d64af539ff3841d23cc869c50bea5c342e032324
chnyangs <xwy411@gmail.com>
2023-11-20 14:02:28 +1100

    add no-cros mode for local fetch

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Load header
-  //   fetch("_includes/header.html")
-  //     .then((response) => response.text())
-  //     .then((data) => {
-  //       document.getElementById("header-placeholder").innerHTML = data;
-  //     });
-
-  // Load footer
   fetch(
-    "https://github.com/ProvSec2024/ProvSec2024/blob/main/_includes/footer.html"
+    "https://github.com/ProvSec2024/ProvSec2024/blob/main/_includes/header.html",
+    { mode: "no-cors" }
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    });
+
+  // Load footer set no-cors to avoid CORS error
+  fetch(
+    "https://github.com/ProvSec2024/ProvSec2024/blob/main/_includes/footer.html",
+    { mode: "no-cors" }
   )
     .then((response) => response.text())
     .then((data) => {
