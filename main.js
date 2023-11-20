60655248df98fdefb12eabca02cf3a24f9ba9930
chnyangs <xwy411@gmail.com>
2023-11-20 16:15:54 +1100

    update partial pages

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Load header
-  fetch(
-    "https://github.com/ProvSec2024/ProvSec2024/blob/main/_includes/header.html",
-    { mode: "no-cors" }
-  )
+  fetch("partial/header.html", { mode: "no-cors" })
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header").innerHTML = data;
     });
 
   // Load footer set no-cors to avoid CORS error
-  fetch(
-    "https://github.com/ProvSec2024/ProvSec2024/blob/main/_includes/footer.html",
-    { mode: "no-cors" }
-  )
+  fetch("partial/footer.html", { mode: "no-cors" })
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer").innerHTML = data;
