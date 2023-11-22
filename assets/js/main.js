d0a295039337c0e3cdbe0184da337ec7b6f03e00
Xiangwen <xwy411@gmail.com>
2023-11-22 16:00:00 +1100

    update config for cfp page

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,18 +12,25 @@ $(document).ready(function () {
     .then((data) => {
       document.getElementById("footer").innerHTML = data;
     });
-
   $.getJSON("config/data.json", { mode: "no-cors" })
     .done(function (config) {
-      // Get title, subtitle, location, date, and hoster from the JSON file
+      //-- setting index.html -- Get title, subtitle, location, date, and hoster from the JSON file
       const index_json = config["index"];
-
       // Set title, subtitle, location, date, and hoster to HTML by id
       document.getElementById("title").innerText = index_json["title"];
       document.getElementById("subtitle").innerText = index_json["subtitle"];
       document.getElementById("location").innerText = index_json["location"];
       document.getElementById("date").innerText = index_json["date"];
       document.getElementById("hoster").innerText = index_json["hoster"];
+      // Setting CFP page
+      const cfp_json = config["cfp"];
+      document.getElementById("cfp_sub_date").innerText = cfp_json["sub_date"];
+      document.getElementById("cfp_notification_date").innerText =
+        cfp_json["notification_date"];
+      document.getElementById("cfp_camera_ready_date").innerText =
+        cfp_json["camera_ready_date"];
+      document.getElementById("cfp_conference_date").innerText =
+        cfp_json["conference_date"];
     })
     .fail(function (jqxhr, textStatus, error) {
       console.log("Error reading JSON file: " + error);
