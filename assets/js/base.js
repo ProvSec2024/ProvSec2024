70f5556fb2a8d9c853a182ff84e4fe81efa9f7c8
chnyangs <xwy411@gmail.com>
2024-08-17 20:42:03 +1000

    add keynotes & program pages

---
+++ assets/js/base.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // Load header
+  fetch("partial/header.html", { mode: "no-cors" })
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  // Load footer set no-cors to avoid CORS error
+  fetch("partial/footer.html", { mode: "no-cors" })
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
